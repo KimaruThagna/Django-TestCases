f03391f680cee15a8203be11eb942542e6e1ae75
Kimaru thagana <macbuntu@localhost.localdomain>
2020-11-11 10:49:35 +0300

    add model admin as per #3

--- wallets/admin.py
+++ wallets/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import WalletOwner, DigitalWallet
 
-# Register your models here.
+
+@admin.register(DigitalWallet)
+class DigitalWalletAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'iban', 'balance')
+    list_display_links = ('owner',)
+    list_per_page = 25
+    search_fields = ('iban','owner')
+    list_filter = ('balance',)
+
+
+@admin.register(WalletOwner)
+class WalletOwnerAdmin(admin.ModelAdmin):
+    search_fields = ('email',)
+    list_per_page = 25
+    list_display_links = ('email',)
+    list_display = ('given_name','family_name','email')
+    list_filter = ('family_name','given_name','email')
+
+
