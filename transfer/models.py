c503a063ea023308089b5ae517cbaacb040b8b87
Kimaru thagana <macbuntu@192.168.1.7>
2020-11-11 17:45:00 +0300

    add notify signal upon save

--- transfer/models.py
+++ transfer/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from wallets.models import DigitalWallet
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.conf import settings
 
 class Transaction(models.Model):
     sender = models.ForeignKey(
@@ -21,4 +23,16 @@ class Transaction(models.Model):
         max_digits=10, decimal_places=2, verbose_name=_("Value")
     )
     created = models.DateTimeField(auto_now_add=True, verbose_name=_("Created"))
+
+
+@receiver(post_save, sender=Transaction)
+def transaction_created(sender, instance, created, **kwargs):
+    if created:
+        if instance.value >= int(settings.TRANSACTION_NOTIFY_LIMIT_INBOUND):
+            # send notification to receiver of funds
+            instance.recepient.owner.notify_about_transaction(instance)
+
+        if instance.value <= int(settings.TRANSACTION_NOTIFY_LIMIT_OUTBOUND):
+            # send notification to sender
+            instance.sender.owner.notify_about_transaction(instance)
 
