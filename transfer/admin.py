f6343b7533b42a48ca5d54abfd92ea41bab645be
Kimaru thagana <macbuntu@localhost.localdomain>
2020-11-11 11:51:08 +0300

    add notification method

--- transfer/admin.py
+++ transfer/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Transaction
+from admin_auto_filters.filters import AutocompleteFilter
 
-# Register your models here.
+class SenderFilter(AutocompleteFilter):
+    title = 'Digital Wallet Owner'
+    field_name = 'sender'
+
+class RecepientFilter(AutocompleteFilter):
+    title = 'Digital Wallet Owner'
+    field_name = 'recepient'
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_filter = ('value', 'created', 'sender', 'recepient')
+    list_display_links = ('created',)
+    list_display = ('sender', 'recepient', 'value', 'created')
+    list_per_page = 50
+    search_fields = ('created',SenderFilter,RecepientFilter)
+    list_select_related = ('sender', 'recepient')
+
+# def queryset(self, request):
+    #     return super(DigitalWalletAdmin, self).get_queryset(request).prefetch_related('owner')
--- wallets/models.py
+++ wallets/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from django.conf import settings
+from django.core.mail import send_mail
 
 class WalletOwner(models.Model):
     given_name = models.CharField(max_length=50, verbose_name=_("Given name"))
@@ -25,4 +27,11 @@ class DigitalWallet(models.Model):
     def __str__(self):
         return f'{self.owner.given_name} has {self.balance} digital coins'
 
-
+    def notify_about_transaction(self, transaction):
+        send_mail(
+            settings.NOTIFICATION_SUBJECT,
+            f'This is to notify you of a transaction performed on{transaction.created} of value {transaction.value}',
+            settings.DEFAULT_SENDER,
+            [self.email],
+            fail_silently=False,
+        )
