2372e12d73767ea144cf80fe4bc00f2f7015456b
Kimaru thagana <macbuntu@localhost.localdomain>
2020-11-11 11:15:16 +0300

    add filter functionality for model admin #3

--- django_tests/settings.py
+++ django_tests/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'transfer',
     'wallets',
+    'admin_auto_filters',
 ]
 
 MIDDLEWARE = [
--- wallets/admin.py
+++ wallets/admin.py
@@ -1,22 +1,26 @@
 from django.contrib import admin
 from .models import WalletOwner, DigitalWallet
+from admin_auto_filters.filters import AutocompleteFilter
 
+class WalletOwnerFilter(AutocompleteFilter):
+    title = 'Digital Wallet Owner'
+    field_name = 'owner'
 
 @admin.register(DigitalWallet)
 class DigitalWalletAdmin(admin.ModelAdmin):
     list_display = ('owner', 'iban', 'balance')
     list_display_links = ('owner',)
     list_per_page = 25
-    search_fields = ('iban','owner')
-    list_filter = ('balance',)
+    search_fields = ('iban','owner__email','owner__given_name','owner__family_name')
+    list_filter = ('balance',WalletOwnerFilter)
 
 
 @admin.register(WalletOwner)
 class WalletOwnerAdmin(admin.ModelAdmin):
-    search_fields = ('email',)
+    search_fields = ('email','given_name','family_name')
     list_per_page = 25
     list_display_links = ('email',)
     list_display = ('given_name','family_name','email')
-    list_filter = ('family_name','given_name','email')
+    list_filter = ('email',)
 
 
